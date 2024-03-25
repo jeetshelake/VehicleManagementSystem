4e89e8efd1d386f0176447aaac85f966070f2ebe
vishwajit shelake <vishwajitshelake22@gmail.com>
2024-03-25 15:44:18 +0530

    empty app is created without thirdparty libraries installation

---
+++ VehicleManagement/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Logins from './components/login folder/Logins'
+
+function App() {
+
+  return <>
+    <Logins />
+  </>
+}
+
+export default App
---
+++ VehicleManagement/src/components/login folder/Logins.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Logins.css"
+
+const Logins = () => {
+  return<>
+    <div>Logins</div>
+  </>
+}
+
+export default Logins
